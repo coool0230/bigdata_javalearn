1994aaa09013cd2e920703edd83f441b48b61ccb
huyn.huimeng.G5400 <coool0230@163.com>
2019-01-10 18:13:47 +0800

    G5400 day10 0110 task

---
+++ src/day/ten/com/ErJinZhi.java
@@ -0,0 +1,13 @@
+package day.ten.com;
+
+class ErJinZhi{
+	public static void main(String[] args){
+		Binary(13);
+	}
+
+	public static void Binary(int num) {
+		for (int i = 31; i >= 0; i--) {
+			System.out.print(num >> i & 1);
+		}
+	}
+}
